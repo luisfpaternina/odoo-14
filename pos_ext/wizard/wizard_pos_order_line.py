b7a7e99422ccf9d7ba35db621bd4e86787baf7f1
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-11-28 20:13:13 -0500

    add

---
+++ pos_ext/wizard/wizard_pos_order_line.py
@@ -0,0 +1,10 @@
+from odoo import fields, models, api
+from datetime import datetime
+
+class WizardPosOrderLine(models.TransientModel):
+    _name = "pos.order.line.wizard"
+    _description = "Pos order lie wizard"
+
+    line_id = fields.Many2one(
+        'pos.order.line',
+        string="Line")
