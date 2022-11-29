12d03c25d3deef09ec7eae34d9817e61e381d637
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-11-28 19:43:36 -0500

    update

---
+++ pos_ext/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'PoS extended',
+
+    'version': '14.0.0.0',
+
+    'author': "Luis Felipe Paternina",
+
+    'contributors': ['Luis Felipe Paternina'],
+
+    'website': "",
+
+    'category': 'PoS',
+
+    'depends': [
+
+        'base',
+        'base_automation',
+        'point_of_sale',
+
+    ],
+
+    'data': [
+       
+        #'views/res_company.xml',
+                   
+    ],
+    'installable': True
+}
---
+++ pos_ext/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from . import res_company
+from . import account_move_unpaid
+from . import account_move
+from . import account_move_line
+from . import res_partner
+from . import res_config_settings
+from . import project_task
+from . import mrp_bom
