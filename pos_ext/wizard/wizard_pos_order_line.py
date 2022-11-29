416921544716e40b0fb36e7c3be524b8c8aee3df
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-11-28 20:34:54 -0500

    add

--- pos_ext/wizard/wizard_pos_order_line.py
+++ pos_ext/wizard/wizard_pos_order_line.py
@@ -11,3 +11,10 @@ class WizardPosOrderLine(models.TransientModel):
     tax_ids = fields.Many2many(
         'account.tax',
         string="Taxes")
+
+    def update_taxes(self):
+        ids = []
+        for record in self:
+            if not record.tax_ids_after_fiscal_position:
+                ids.append(record.tax_ids.ids)
+                record.write({'tax_ids_after_fiscal_position': [(6, 0 , ids)]})
