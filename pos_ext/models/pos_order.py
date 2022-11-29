d93fec2e6bb8f3a41e017445c5058221cc35c334
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-11-28 20:40:41 -0500

    Update pos_order.py

--- pos_ext/models/pos_order.py
+++ pos_ext/models/pos_order.py
@@ -8,16 +8,9 @@ class PosOrderLine(models.Model):
         'account.tax',
         string="Taxes")
 
-    def update_taxes(self):
-        ids = []
-        for record in self:
-            if not record.tax_ids_after_fiscal_position:
-                ids.append(record.tax_ids.ids)
-                record.write({'tax_ids_after_fiscal_position': [(6, 0 , ids)]})
-
     def action_done_show_wizard(self):
        return {'type': 'ir.actions.act_window',
-               'name': _('Add taxes'),
+               'name': _('Update taxes'),
                'res_model': 'pos.order.line.wizard',
                'target': 'new',
                'view_id': self.env.ref('pos_ext.pos_order_line_wizard').id,
