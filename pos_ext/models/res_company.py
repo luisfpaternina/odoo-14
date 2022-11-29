3d4073522c04100196c7dd9af09f662b40eef368
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-11-28 19:45:15 -0500

    add

--- pos_ext/models/res_company.py
+++ pos_ext/models/res_company.py
@@ -2,22 +2,3 @@ from odoo import models, fields, api, _
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
-
-    terms = fields.Text(
-        string="Terms")
-    show_technical = fields.Boolean(
-        string="Enable technical")
-    is_potencial_client = fields.Boolean(
-        string="Is potencial client")
-    has_rae = fields.Boolean(
-        string="Has RAE")
-    is_forecast_made = fields.Boolean(
-        string="Is forecast made")
-    user_operator_id = fields.Many2one(
-        'hr.employee',
-        string="Users")
-    is_qr_required = fields.Boolean(
-        string="Is QR required?")
-    is_minute_point = fields.Boolean(
-        string="Enable minute point")
-    
