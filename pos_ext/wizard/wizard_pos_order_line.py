4eef5268029d487e20ea70e29444d2bb81ef7e25
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-11-29 13:16:21 -0500

    Update wizard_pos_order_line.py

--- pos_ext/wizard/wizard_pos_order_line.py
+++ pos_ext/wizard/wizard_pos_order_line.py
@@ -18,9 +18,9 @@ class WizardPosOrderLine(models.TransientModel):
         string="Taxes")
 
     def update_taxes(self):
-        pedido = env['pos.order'].browse(self.order_id)
+        pedido = self.env['pos.order'].browse(self.order_id)
         logging.info('--------------------------------------')
         logging.info(pedido)
-        iva = env['account.tax'].browse(19)
+        iva = self.env['account.tax'].browse(19)
         linea = pedido.lines[7]
         linea.write({'tax_ids':iva})
