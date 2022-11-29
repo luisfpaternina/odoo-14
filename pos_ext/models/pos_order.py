70ac9b4b79ca03d531d7fb6dcf35fb37a19d0a9c
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-11-28 20:30:15 -0500

    wzv

--- pos_ext/models/pos_order.py
+++ pos_ext/models/pos_order.py
@@ -14,3 +14,13 @@ class PosOrderLine(models.Model):
             if not record.tax_ids_after_fiscal_position:
                 ids.append(record.tax_ids.ids)
                 record.write({'tax_ids_after_fiscal_position': [(6, 0 , ids)]})
+
+    def action_done_show_wizard(self):
+       return {'type': 'ir.actions.act_window',
+               'name': _('Add taxes'),
+               'res_model': 'pos.order.line.wizard',
+               'target': 'new',
+               'view_id': self.env.ref('pos_ext.pos_order_line_wizard').id,
+               'view_mode': 'form',
+               'context': {}
+               }
